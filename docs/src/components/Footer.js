b70bae96318f38ebe1f9e588b4293190dd436737
Alicia <aliciasun0102@gmail.com>
2025-01-19 04:32:49 -0500

    z

--- docs/src/components/Footer.js
+++ docs/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="bg-dark text-light text-center py-3">
-    <div>© 2025 DevTool Documentation. All Rights Reserved.</div>
+    <div>© 2025 piplog Documentation. All Rights Reserved.</div>
   </footer>
 );
 
