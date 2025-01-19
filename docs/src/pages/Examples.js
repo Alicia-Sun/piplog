171bffbadd3e6877420ad5a021290c19358cd783
Alicia <aliciasun0102@gmail.com>
2025-01-18 22:45:04 -0500

    edited examples

--- docs/src/pages/Examples.js
+++ docs/src/pages/Examples.js
@@ -12,26 +12,28 @@ const Examples = () => (
             </pre>
             <p>This command creates a timestamped record of all installed packages at the current moment. It writes this record to a file called ".piplog" where it can be viewed.</p>
             <h2>Compare Package Versions</h2>
-            <p>Similar to git diff, you can view package discrepencies between versions with piplog diff. [version1] and [version2] are optional arguments that can be passed in. If no additional arguments are given, it automatically compares the differences between the current and previous version. If only [version1] is given, it prints the package differences between [version1] and the current version. If both are given, it prints the differences between the two. 
-            </p>
             <pre className="command-block">
             <code>$ piplog diff [version1] [version2]</code>
             </pre>
-            <p>
-            View differences between two saved versions, including added and removed packages.
+            <p>Similar to git diff, you can view package discrepencies between versions with piplog diff. [version1] and [version2] are optional arguments that can be passed in. If no additional arguments are given, it automatically compares the differences between the current and previous version. If only [version1] is given, it prints the package differences between [version1] and the current version. If both are given, it prints the differences between the two. 
             </p>
             <h2>Restore to a Previous Version</h2>
-            <pre>
-            <code>python piplog.py restore 2023-01-01T12:00:00</code>
+            <pre className="command-block">
+            <code>$ python piplog.py restore [version]</code>
             </pre>
             <p>
-            This command uninstalls and installs packages to match the saved state from the specified timestamp.
+            This command restores the packages and versions to the specified version. [version] is optional and if not specified, simply reverts one version back.
             </p>
             <h2>Execute a Pip Command with History</h2>
             <pre>
-            <code>python piplog.py pip install pandas</code>
+            <code>$ piplog pip ...</code>
             </pre>
-            <p>This installs pandas and automatically saves the new package state.</p>
+            <p>This is a wrapper for pip. It executes the pip command but saves a snapshot of the packages after the pip operation in the version history.</p>
+            <h2>Clear piplog History</h2>
+            <pre>
+            <code>$ piplog clear</code>
+            </pre>
+            <p>Clears the file that piplog uses to track version history.</p>
         </div>
     </div>
 );
